7ff3533ff2823a5dc6a5a5cc2d86f0b226a336fa
Li, Sheng-Wei <pha123661@gmail.com>
2022-09-30 20:02:09 +0800

    Add CNN layer

--- hw1/model.py
+++ hw1/model.py
@@ -55,11 +55,35 @@ class SeqClassifier(torch.nn.Module):
 
 
 class SeqTagger(SeqClassifier):
+    def __init__(self, embeddings: torch.tensor, hidden_size: int, num_layers: int, dropout: float, bidirectional: bool, num_class: int) -> None:
+        super().__init__(embeddings, hidden_size,
+                         num_layers, dropout, bidirectional, num_class)
+        # add a 2-layer CNN
+        embed_dim = embeddings.shape[1]
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(embed_dim, embed_dim,
+                      kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv1d(embed_dim, embed_dim,
+                      kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+        )
 
     def forward(self, ids, lengths) -> Dict[str, torch.Tensor]:
         embeds = self.embed(ids)
-        embeds = pack_padded_sequence(embeds, lengths, batch_first=True)
 
+        # feed x into CNN
+        # CNN input: (b, h, seq_len)
+        embeds = torch.swapaxes(embeds, -1, -2)
+        embeds = self.conv1(embeds)
+        embeds = self.conv2(embeds)
+        embeds = torch.swapaxes(embeds, -1, -2)
+
+        embeds = pack_padded_sequence(embeds, lengths, batch_first=True)
         x, h = self.net(embeds)
         x, lens = pad_packed_sequence(x, batch_first=True)
         # x.shape = (b, seq_len, h_size)
