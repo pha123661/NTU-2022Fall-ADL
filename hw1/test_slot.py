5c06b0e85abe968747a160092f88b4f7fac13a2c
Li, Sheng-Wei <pha123661@gmail.com>
2022-10-01 16:05:17 +0800

    Fix typo

--- hw1/test_slot.py
+++ hw1/test_slot.py
@@ -87,7 +87,7 @@ def parse_args() -> Namespace:
     # model
     parser.add_argument("--hidden_size", type=int, default=512)
     parser.add_argument("--num_layers", type=int, default=2)
-    parser.add_argument("--dropout", type=float, default=0.1)
+    parser.add_argument("--dropout", type=float, default=0.2)
     parser.add_argument("--bidirectional", type=bool, default=True)
 
     # data loader
